59d2be38f897b529546d5b3b46543a4753b60014
KelyMac <90709315+KelyMac@users.noreply.github.com>
2022-05-02 19:28:54 -0400

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,28 @@
+/**
+* Contains a method <code>void display()</code> that all the <code>main</code>
+* methods call to show the student information. Fill the box with your personal
+* information.
+*
+* @author Marcel Turcotte (marcel.turcotte@uottawa.ca)
+*/
+
+public class StudentInfo {
+
+    /**
+    * Displays the student information: student name, id, section, etc for each
+    * member of the team.
+    */
+
+    public static void display() {
+
+        System.out.println("************************************************************");
+        System.out.println("*                  Kuan-Yu Chang         Jiayi Ma          *");
+        System.out.println("*                  300201058             300263220         *");
+        System.out.println("*                  ITI1121               ITI1121           *");
+        System.out.println("*                  C-2                   C-4               *");
+        System.out.println("************************************************************");
+        System.out.println();
+
+    }
+
+}
